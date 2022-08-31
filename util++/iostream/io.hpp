032924d18479d9c1404181dc6372b31db6a500ba
Lizard <lizardwine@hotmail.com>
2022-08-31 00:54:15 -0500

    add inputs fucntions

--- util++/iostream/io.hpp
+++ util++/iostream/io.hpp
@@ -1,13 +1,78 @@
 #include <iostream>
 #include <vector>
-
+#include <cstdlib>
 using namespace std;
+
+
+string input(string message){
+    cout<<message;
+    string var;
+    cin>>var;
+    return var;
+}
+int inputI(string message){
+    cout<<message;
+    int var;
+    cin>>var;
+    return var;
+}
+float inputF(string message){
+    cout<<message;
+    float var;
+    cin>>var;
+    return var;
+}
+double inputD(string message){
+    cout<<message;
+    double var;
+    cin>>var;
+    return var;
+}
+
+const char* inputCCa(string message){
+    const char* var = input(message).c_str();
+    return var;
+}
+char inputC(string message){
+    cout<<message;
+    char var;
+    cin>>var;
+    return var;
+}
+char* inputCa(string message){
+    cout<<message;
+    char* var;
+    cin>>var;
+    return var;
+}
+unsigned int inputUI(string message){
+    cout<<message;
+    unsigned int var;
+    cin>>var;
+    return var;
+}
+unsigned long inputUL(string message){
+    cout<<message;
+    unsigned long var;
+    cin>>var;
+    return var;
+}
+
+long inputL(string message){
+    cout<<message;
+    long var;
+    cin>>var;
+    return var;
+}
 
 class print{
     public:
         print(const char *toPrint,string end = "\n"){
             cout << toPrint<<end;
         }
+	print(char toPrint,string end = "\n"){
+	    cout << toPrint<<end;
+	}
         print(string toPrint,string end = "\n"){
             cout << toPrint<<end;
         }
